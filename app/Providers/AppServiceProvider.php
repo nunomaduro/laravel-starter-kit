de1c6b448a867cca80f2cf845c4b5ee9d22cb06d
Nuno Maduro <enunomaduro@gmail.com>
2026-01-24 18:41:50 +0000

    fix: use xdebug mode on coverage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
