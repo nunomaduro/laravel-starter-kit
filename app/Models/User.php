81a38e785afb74451b3bfe2341aa423e22a6e0dc
Josh Donnell <joshua.donnell1@gmail.com>
2026-02-28 09:38:04 +0000

    chore: deps update and rector fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Override;
 
 /**
  * @property-read string $id
@@ -33,6 +34,7 @@ final class User extends Authenticatable implements MustVerifyEmail
     /**
      * @var list<string>
      */
+    #[Override]
     protected $hidden = [
         'password',
         'remember_token',
